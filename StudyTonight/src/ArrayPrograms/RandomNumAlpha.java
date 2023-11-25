b68187987989bd2712ea054c602aef3fc8361abc
fahim.abbas <fahim.abbas@P2ELT22.P2E.COM>
2023-11-25 12:58:48 +0530

    Initial Commit

---
+++ StudyTonight/src/ArrayPrograms/RandomNumAlpha.java
@@ -0,0 +1,16 @@
+package ArrayPrograms;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import java.util.Random;
+import java. util. UUID;
+
+public class RandomNumAlpha 
+{
+	String random_email=RandomStringUtils.random(8, "0123456789abcdefghijkl");
+	
+	public void result()
+	{
+	System.out.println(random_email);
+	
+	}
+}
---
+++ StudyTonight/src/ArrayPrograms/array_bound_check.java
@@ -0,0 +1,5 @@
+package ArrayPrograms;
+
+public class array_bound_check {
+
+}
